48cee119f58af6176b405c184d931fbf4696cfdf
VSushkov <resid.marketing@gmail.com>
2023-04-28 00:51:01 +0300

    Added modals

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     //Мобільне меню
-    const mobMenu = document.querySelector('.mobile-menu');
+    const mobMenu = document.querySelector('.header');
     const burger = document.querySelector('.hamburger');
-    const mobLinks = document.querySelectorAll('.mobile-menu__list-item');
+    const mobLinks = document.querySelectorAll('.menu__link');
     let mobMenuActive = false;
 
     burger.addEventListener('click', (e) => {
         if (mobMenuActive) {
-            burger.classList.remove('hamburger-active');
+            burger.classList.remove('hamburger__icon-active');
             mobMenu.style.left = "-100%";
             mobMenuActive = false;
             document.body.style.overflow = '';
         } else {
-            burger.classList.add('hamburger-active');
+            burger.classList.add('hamburger__icon-active');
             mobMenu.style.left = 0;
             mobMenuActive = true;
             document.body.style.overflow = 'hidden';
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     mobLinks.forEach(el => el.addEventListener('click', () => {
         if (mobMenuActive) {
-            burger.classList.remove('hamburger-active');
+            burger.classList.remove('hamburger__icon-active');
             mobMenu.style.left = "-100%";
             mobMenuActive = false;
             document.body.style.overflow = '';
@@ -30,8 +30,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
 
     // SELECT FORMS
-    const selectHeader = document.querySelectorAll('.order__form-category-header');
-    const selectItem = document.querySelectorAll('.order__form-category-item');
+    const selectHeader = document.querySelectorAll('.order__form-select-header');
+    const selectItem = document.querySelectorAll('.order__form-select-item');
     const selectCategoryBody = document.getElementById('select-category');
     const selectServicesBody = document.getElementById('select-services');
 
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function selectChoose() {
         let item = this.innerHTML,
-            currentHTML = this.closest('.order__form-category').querySelector('.order__form-category-current');
+            currentHTML = this.closest('.order__form-select').querySelector('.order__form-select-current');
         currentHTML.innerHTML = item;
 
         this.parentElement.parentElement.classList.remove('is-active');
@@ -69,4 +69,44 @@ document.addEventListener('DOMContentLoaded', () => {
         selectCategoryBody.classList.remove('is-active');
     }
 
+
+
+    // MODALS
+    const overlay = document.querySelector('.overlay');
+    const closeButtons = document.querySelectorAll('.modal__close-icon');
+    const buttons = document.querySelectorAll('.button');
+    // const closeButton = document.querySelector('.modal__close-icon');
+
+    buttons.forEach(function (item) {
+        if (item.hasAttribute('data-modal')) {
+            item.addEventListener('click', function (e) {
+                e.preventDefault();
+                const attr = this.getAttribute('data-modal'),
+                    modalWindow = document.getElementById(`${attr}`);
+
+                modalWindow.classList.add('show');
+                overlay.classList.add('show');
+                document.body.style.overflow = 'hidden';
+                document.body.style.paddingRight = '12px';
+            });
+        }
+    });
+
+    closeButtons.forEach(function (item) {
+        item.addEventListener('click', function () {
+            const modal = this.closest('.modal');
+
+            modal.classList.remove('show');
+            overlay.classList.remove('show');
+            document.body.style.overflow = '';
+            document.body.style.paddingRight = '';
+        });
+    });
+
+    overlay.addEventListener('click', function () {
+        document.querySelector('.modal.show').classList.remove('show');
+        this.classList.remove('show');
+        document.body.style.overflow = '';
+        document.body.style.paddingRight = '';
+    });
 });
